2d939c49fdc2cb3590daeb0d2c8336c490cb508a
iamsitting <carlos@iamsitting.com>
2025-05-08 13:50:33 -0500

    update

--- app/entry.tsx
+++ app/entry.tsx
@@ -4,13 +4,13 @@ import routes from './routes';
 import App from './root';
 
 // Handle GitHub Pages 404 redirects
-const pathSegmentsToKeep = 0;
-const l = window.location;
-if (l.pathname.includes('/?/')) {
-  const newPath = l.pathname.slice(1).split('/?/')[1].replace(/~and~/g, '&');
-  const newSearch = l.search.slice(1).replace(/~and~/g, '&');
-  const newUrl = l.pathname.split('/?/')[0] + '/' + newPath + (newSearch ? '?' + newSearch : '') + l.hash;
-  window.history.replaceState(null, '', newUrl);
+const redirect = sessionStorage.getItem('redirect');
+if (redirect) {
+  sessionStorage.removeItem('redirect');
+  const newPath = redirect.split('/?/')[1]?.replace(/~and~/g, '&');
+  if (newPath) {
+    window.history.replaceState(null, '', '/' + newPath);
+  }
 }
 
 const router = createBrowserRouter([
